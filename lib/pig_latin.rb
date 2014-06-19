34aa424774a12ab3c7a2c940496bf9bf9366dbc1
Gideon Wulfsohn <gideon.wulfsohn@gmail.com>
2014-06-19 12:35:24 -0700

    update

--- lib/pig_latin.rb
+++ lib/pig_latin.rb
@@ -1,7 +1,6 @@
 require "pig_latin/version.rb"
 
 module PigLatin
-  # Your code goes here...
 end
 
 require_relative 'pig_latin/version.rb'
--- lib/pig_latin/regex.rb
+++ lib/pig_latin/regex.rb
@@ -26,36 +26,3 @@ class PigLatin::Regex
     /\A[^aeiouy]+/.match(word)
   end
 end
-
-# require 'pry-debugger'
-# isaplpha
-
-# class PigLatin::Regex
-#   def translate(words)
-#     pig_latin_words_array= []
-#     punctionation = []
-#     punctuation_index = 0
-#     words = words.split
-#     words.each do |word|
-#       if /\y/.match(word)
-#         if /[^aeiou]/.match(word)
-#         end
-#       elsif /\A[^aeiou]/.match(word)
-#         if word.match(/^[[:alpha:]]+$/) #check if word has punctuation
-#           punctuation_location = 0
-#           word.each do |letter|
-#             if word.match(/^[[:alpha:]]+$/)
-#               punctuation[punctuation_index] = punctionation[]
-#             end
-#             punctuation_location += 1
-#           end
-#         word + s + "ay"
-#       else
-#         word = word + 'way'
-#         pig_latin_words_array << word
-#       end
-#       punctuation_index += 1
-#     end
-#     pig_latin_words_array
-#   end
-# end
